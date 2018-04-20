7dacb7176510f1209c0de9341b5fa8af8adb6f78
ShankarR <ShankarR@NPF0SKJU9.sqs.group.intl>
2018-04-20 12:47:21 +0530

    Added new files

---
+++ src/algorithms/ApplesandOrange.java
@@ -0,0 +1,73 @@
+package algorithms;
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class ApplesandOrange {
+
+	/*
+	 * Complete the countApplesAndOranges function below.
+	 */
+	static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+		/*
+		 * Write your code here.
+		 */
+		int applesInRange = 0;
+		int orangesInRange = 0;
+		for(int apple: apples){
+			if(a + apple >= s && a + apple <= t ) applesInRange++;
+		}
+		
+		for(int orange: oranges){
+			if(b + orange >= s && b + orange <= t ) orangesInRange++;
+		}
+		System.out.println(applesInRange);
+		System.out.println(orangesInRange);
+
+	}
+
+	private static final Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		String[] st = scan.nextLine().split(" ");
+
+		int s = Integer.parseInt(st[0].trim());
+
+		int t = Integer.parseInt(st[1].trim());
+
+		String[] ab = scan.nextLine().split(" ");
+
+		int a = Integer.parseInt(ab[0].trim());
+
+		int b = Integer.parseInt(ab[1].trim());
+
+		String[] mn = scan.nextLine().split(" ");
+
+		int m = Integer.parseInt(mn[0].trim());
+
+		int n = Integer.parseInt(mn[1].trim());
+
+		int[] apple = new int[m];
+
+		String[] appleItems = scan.nextLine().split(" ");
+
+		for (int appleItr = 0; appleItr < m; appleItr++) {
+			int appleItem = Integer.parseInt(appleItems[appleItr].trim());
+			apple[appleItr] = appleItem;
+		}
+
+		int[] orange = new int[n];
+
+		String[] orangeItems = scan.nextLine().split(" ");
+
+		for (int orangeItr = 0; orangeItr < n; orangeItr++) {
+			int orangeItem = Integer.parseInt(orangeItems[orangeItr].trim());
+			orange[orangeItr] = orangeItem;
+		}
+
+		countApplesAndOranges(s, t, a, b, apple, orange);
+	}
+}
---
+++ src/algorithms/BetweenTwoSets.java
@@ -0,0 +1,89 @@
+package algorithms;
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class BetweenTwoSets {
+
+	/*
+	 * Complete the getTotalX function below.
+	 */
+	static int getTotalX(int[] a, int[] b) {
+		/*
+		 * Write your code here.
+		 */
+		int aMax = -1;
+		int bMin = 10000;
+		for(int i: a){
+			if(i>aMax) aMax = i;
+		}
+
+		for(int j: b){
+			if(j<bMin) bMin = j;
+		}
+
+		boolean bFlag = false;
+		int totCount=0; 
+
+		for(int num=aMax; num<=bMin; num++){
+			for(int i: a){
+				if(num%i == 0) bFlag = true;
+				else{
+					bFlag = false;
+					break;
+				}
+			}
+			if(bFlag){
+				for(int j: b){
+					if(j%num ==0) bFlag = true;
+					else{
+						bFlag = false;
+						break;
+					}
+				}
+			}
+			if(bFlag == true) totCount++;
+		}
+		return totCount;
+	}
+
+	private static final Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		//		BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		String[] nm = scan.nextLine().split(" ");
+
+		int n = Integer.parseInt(nm[0].trim());
+
+		int m = Integer.parseInt(nm[1].trim());
+
+		int[] a = new int[n];
+
+		String[] aItems = scan.nextLine().split(" ");
+
+		for (int aItr = 0; aItr < n; aItr++) {
+			int aItem = Integer.parseInt(aItems[aItr].trim());
+			a[aItr] = aItem;
+		}
+
+		int[] b = new int[m];
+
+		String[] bItems = scan.nextLine().split(" ");
+
+		for (int bItr = 0; bItr < m; bItr++) {
+			int bItem = Integer.parseInt(bItems[bItr].trim());
+			b[bItr] = bItem;
+		}
+
+		int total = getTotalX(a, b);
+
+		System.out.println(String.valueOf(total));
+		//		bw.newLine();
+		//
+		//		bw.close();
+	}
+}
